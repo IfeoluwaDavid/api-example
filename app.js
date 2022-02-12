d5e366b2ec2b3f04f6d9131044900b6fae1926e9
IfeoluwaDavid <ifeoluwaadese@ymail.com>
2022-02-11 19:32:30 -0500

    Clean up & Setup Routes

--- app.js
+++ app.js
@@ -5,13 +5,9 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var apiRouter = require("./routes/api");
 
 var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -20,7 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -37,11 +33,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-
-// const PORT = 3000;
-
-// // app.listen(PORT, () => {
-// //   console.log(`api-example is currently listening on port ${PORT}`);
-// // });
 
 module.exports = app;
---
+++ routes/api.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/ping", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/posts", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+module.exports = router;
